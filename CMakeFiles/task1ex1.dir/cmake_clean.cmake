dd797b2e59a106e03ba4b9ae20207683b42785a8
root <root@BraveVesperia.localdomain>
2018-01-18 21:55:26 +0000

    initial commit

---
+++ CMakeFiles/task1ex1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task1ex1.dir/task1ex1.c.o"
+  "task1ex1.pdb"
+  "task1ex1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/task1ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/task1ex3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task1ex3.dir/task1ex3.c.o"
+  "task1ex3.pdb"
+  "task1ex3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/task1ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
