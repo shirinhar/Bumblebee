6945e8991f7079a0b0a4d5928f5b060742657975
shirinhar <32335494+shirinhar@users.noreply.github.com>
2018-01-19 13:31:56 +0000

    Update task1ex1.c
    a maybe for getting rid of the circles

--- task1ex1.c
+++ task1ex1.c
@@ -10,11 +10,14 @@
 int main(int argc, const char* argv[])
 {
     //drive_goto(1000, 1000);
-    simulator_startNewSmokeTrail();
+    
     for (int i=0; i<4;i++)
     {
+        simulator_startNewSmokeTrail();
         drive_goto(77,77);
+        simulator_topSmokeTrail();
         drive_goto(26,-26);
+        drive_goto(-9,-9);
     }
 
     return 0;
