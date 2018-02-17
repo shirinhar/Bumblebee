713a329773bc15c1531d9c389d23839d49c8ea6d
kimia84 <Kimiapirouzkia@gmail.com>
2018-02-17 01:42:49 +0000

    Update task4.c
    few changes to make the robot navigate smoother.

--- Midterm/task4.c
+++ Midterm/task4.c
@@ -1,39 +1,56 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include "abdrive.h"
+#include "simpletext.h"
 #include "simpletools.h"
 #include "ping.h"
 #include "abdrive.h"
+#include "simulator.h"
 #include "task4.h"
-#include <math.h>
+
 
 
 int main()
 {
-  // get initial variables
-  double left_ir=0;
-  double right_ir =0;
-  double second_ping = ping_cm(8);
 
-  // leave while loop when 5cm away from wall
-  while(ping_cm(8) >= 5 ){
-      
-    drive_speed(50,50);  
-    int tick=0;
-    double diff_ir,first_ping,diff_ping;
-    first_ping = second_ping;
 
-    second_ping = ping_cm(8);
-    left_ir = find_left_ir();
-    right_ir = find_right_ir();
-        
-    diff_ir = (left_ir - right_ir);
 
-    // if robot is not parallel to the wall, enter loop  
-    if (diff_ir != 0 && first_ping != second_ping){
-      //diff_ping = (abs)(first_ping - second_ping);
-      turn(left_ir,right_ir,diff_ping);
-    }
+ double left_ir;
+ double right_ir ;
+ double second_ping ;
+ double closeness = 12;
+ simulator_startNewSmokeTrail();
 
-    drive_speed(20,20);
-    pause(50);
-  }  
+ // leave while loop when 15cm away from wall
+ while(ping_cm(8) > 15 ){
 
+   drive_ramp(25,25);
+
+   int tick=0;
+   double diff_ir,first_ping,diff_ping;
+   first_ping = second_ping;
+
+   second_ping = ping_cm(8);
+   left_ir = find_left_ir();
+   right_ir = find_right_ir();
+
+   diff_ir = (left_ir - right_ir);
+
+
+   if (left_ir < closeness){
+     turn_right(left_ir,right_ir,diff_ping);
+   }
+
+   if (right_ir < closeness){
+     turn_left(left_ir,right_ir,diff_ping);
+   }
+
+   if (ping_cm(8) <= 15){
+     break;
+   }
+ }
+ simulator_stopSmokeTrail();
+ go_back();
+ position();
 }
