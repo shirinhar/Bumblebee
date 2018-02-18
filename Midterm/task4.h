010dabf761195d58b3a7177a5eb586096981cff7
Shirin Harandi <32335494+shirinhar@users.noreply.github.com>
2018-02-18 01:25:29 +0000

    delete extra code & try dead reckoning

--- Midterm/task4.h
+++ Midterm/task4.h
@@ -8,40 +8,55 @@
 #include <stdlib.h>
 #include "ping.h"
 #include "simulator.h"
-int tick = 5;
+int tick = 3,i=0;
+double startX, startY, startZ;
 
 double x, y, z, distance;
-double left_ticks[100],right_ticks[100];
+int left_ticks[250],right_ticks[250];
 int distance_forward;
+int left_tick, right_tick;
 
 void position(){
 
-simulator_getPose(&x, &y, &z);
-distance = sqrt(pow(x,2) + pow(y,2));
+	simulator_getPose(&x, &y, &z);
+	distance = sqrt(pow(x,2) + pow(y,2));
 
-
-printf("Distance: %f\n", distance/32.5);
+	printf("Distance: %f\n", distance/32.5);
 
 }
-// void dead_reckoning(){
-// 	while (1) {
-//     drive_getTicks(int *left, int *right);
-// 		left = left_ticks[]
-// 		right = right_ticks[]
-// 		pause(100);
-// }
+
+void dead_reckoning(){
+	drive_getTicks(&left_ticks[i], &right_ticks[i]);
+	printf("l: %d, r: %d i: %d\n", left_ticks[i],right_ticks[i],i);
+	i++;
+}
+
+void back(){
+	drive_ramp(25,25);
+	//drive_goto(1,1);
+	while (i > -1){		
+		left_tick = left_ticks[i-1] - left_ticks [i-2];
+		right_tick = right_ticks[i-1] - right_ticks [i-2];
+		i = i - 1;
+		drive_goto(right_tick,left_tick);
+		//drive_speed(20,20);
+	}
 }
 
 // change the robots angle
-void turn_left(double left_ir,double right_ir,double diff_ping){
+void turn_left(){
  	drive_speed(-tick,tick);
 }
-void turn_right(double left_ir,double right_ir,double diff_ping){
+
+void turn_right(){
  	drive_speed(tick,-tick);
 }
+
 void go_back(){
 	drive_ramp(0,0);
+	//drive_goto(-18,-18);
 	drive_goto(51,-51);
+	drive_goto(5,5);
 }
 
 // get the ir reading and return it as a double
@@ -66,3 +81,20 @@ double find_right_ir(){
 	}
 	return right_ir;
 }
+
+void get_start_position(){
+	simulator_getPose(&startX, &startY, &startZ);	
+}
+
+void position_from_start(){
+	double endX, endY, endZ;
+	simulator_getPose(&endX, &endY, &endZ);
+
+	printf("start: %lf, %lf, %lf\nend: %lf, %lf, %lf\n", startX, startY, startZ,endX, endY, endZ);
+	distance = sqrt(pow(endZ-startZ,2) + pow(endY-startY,2) + pow(endX-startX,2));
+	printf("Distance: %f\n", distance/32.5);
+}
+
+
+
+
