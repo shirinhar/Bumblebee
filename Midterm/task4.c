7c500db0ddf16a108269a3cecc38e564925cb70a
Shirin Harandi <32335494+shirinhar@users.noreply.github.com>
2018-02-18 01:24:46 +0000

    delete extra code & try dead reckoning

--- Midterm/task4.c
+++ Midterm/task4.c
@@ -7,50 +7,53 @@
 #include "ping.h"
 #include "abdrive.h"
 #include "simulator.h"
-#include "task4.h"
+#include "test4.h"
 
 
 
 int main()
 {
 
+   double left_ir;
+   double right_ir ;
+   double second_ping ;
+   double closeness = 12;
+   get_start_position();
+   simulator_startNewSmokeTrail();
+
+   // leave while loop when 15cm away from wall
+   while(ping_cm(8) > 15 ){
+
+     drive_ramp(25,25);
+
+     int tick=0;
+     double diff_ir,first_ping,diff_ping;
+     first_ping = second_ping;
+
+     second_ping = ping_cm(8);
+     left_ir = find_left_ir();
+     right_ir = find_right_ir();
+     dead_reckoning();
+     diff_ir = (left_ir - right_ir);
 
 
- double left_ir;
- double right_ir ;
- double second_ping ;
- double closeness = 12;
- simulator_startNewSmokeTrail();
+     if (left_ir < closeness){
+       turn_right();
+     }
 
- // leave while loop when 15cm away from wall
- while(ping_cm(8) > 15 ){
+     if (right_ir < closeness){
+       turn_left();
+     }
 
-   drive_ramp(25,25);
-
-   int tick=0;
-   double diff_ir,first_ping,diff_ping;
-   first_ping = second_ping;
-
-   second_ping = ping_cm(8);
-   left_ir = find_left_ir();
-   right_ir = find_right_ir();
-
-   diff_ir = (left_ir - right_ir);
-
-
-   if (left_ir < closeness){
-     turn_right(left_ir,right_ir,diff_ping);
+     if (ping_cm(8) <= 15){
+       break;
+     }
    }
-
-   if (right_ir < closeness){
-     turn_left(left_ir,right_ir,diff_ping);
-   }
-
-   if (ping_cm(8) <= 15){
-     break;
-   }
- }
- simulator_stopSmokeTrail();
- go_back();
- position();
+   simulator_stopSmokeTrail();
+   go_back();
+   pause(10);
+   simulator_startNewSmokeTrail();
+   back();
+   simulator_stopSmokeTrail();
+   position_from_start();
 }
