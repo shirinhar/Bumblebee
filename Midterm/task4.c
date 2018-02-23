e1900f1cb87388274ca96d646082b22fa05eb385
Shirin Harandi <32335494+shirinhar@users.noreply.github.com>
2018-02-23 14:23:28 +0000

    Update task4.c

--- Midterm/task4.c
+++ Midterm/task4.c
@@ -9,15 +9,13 @@
 #include "simulator.h"
 #include "task4.h"
 
-
 int main()
 {
-  
+
    double left_ir;
    double right_ir ;
    double second_ping ;
    double closeness = 12;
-   get_start_position();
    simulator_startNewSmokeTrail();
    drive_speed(30,30);
 
@@ -25,15 +23,13 @@ int main()
    while(ping_cm(8) > 15 ){
 
      drive_ramp(30,30);
-
+     
      int tick=0;
      double diff_ir,first_ping,diff_ping;
      first_ping = second_ping;
 
      second_ping = ping_cm(8);
      
-     //dead_reckoning();
-
      run();
    }
 
@@ -43,5 +39,5 @@ int main()
    simulator_startNewSmokeTrail();
    back();
    simulator_stopSmokeTrail();
-   position_from_start();
+
 }
