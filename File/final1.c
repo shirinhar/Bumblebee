2fca756c486e862d9c636597067cbc686136779e
Shirin Harandi <32335494+shirinhar@users.noreply.github.com>
2018-03-02 21:18:17 +0000

    Create final1.c

---
+++ File/final1.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "simpletools.h"
+#include "abdrive.h"
+#include "ping.h"
+#include "final1.h"
+
+
+int main() {
+
+    while(1){
+        run();
+    }
+    return 0;
+}
