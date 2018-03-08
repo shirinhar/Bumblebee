8fac6fbde538e6ee8a56d932f6fe22269b74f41b
Shirin Harandi <32335494+shirinhar@users.noreply.github.com>
2018-03-08 16:05:40 +0000

    Update final1.h

--- Final/final1.h
+++ Final/final1.h
@@ -12,7 +12,7 @@
 
 
 int x=0, y=0;
-int visitedX[100],visitedY[100];
+int visitedX[100],visitedY[100], posYs[100],posXs[100],i_am_on[100];
 int visitedX_2[100],visitedY_2[100]; 
 int distance_from = 19;
 int visited_index =0, visited_index_2=0;
@@ -20,6 +20,14 @@ int bool = 0;
 int posX=0, posY=1;
 // 1 is y 0 is x
 int what_am_i_on = 1,ticks = 120;
+int back_it_up;
+
+
+
+/*    int visitedX;
+    int visitedY;
+    int i_am_on;
+}*/
 
 int is_visited_1(int valX,int valY){
     for (int i=0; i < visited_index; i++) {
@@ -42,30 +50,34 @@ void change_direction(){
     if (posX>2){
         posX = -1;
     }
-    if (posX <-2){
+    else if (posX <-2){
         posX = 1;
     }
     if (posY>2){
         posY = -1;
     }
-    if (posY <-2){
+    else if (posY <-2){
         posY = 1;
     } 
 }
 
-void turn_right(){
-    drive_goto(-9,-9);        
-    drive_goto(-26,25);
-    drive_goto(9,9);
-    posX++;
-    posY++;
-    change_direction();
+void im_on(){
     if (what_am_i_on == 1){
         what_am_i_on = 0;
     }
     else{
         what_am_i_on = 1;
     }
+}
+
+void turn_right(){
+    drive_goto(-9,-9);        
+    drive_goto(-26,25);
+    drive_goto(9,9);
+    posX--;
+    posY--;
+    change_direction();
+    im_on();
     push(-9,-9);
     push(-26,25);
     push(9,9);
@@ -75,15 +87,10 @@ void turn_left(){
     drive_goto(-9,-9);        
     drive_goto(25,-26);
     drive_goto(9,9);
-    posX--;
-    posY--;
+    posX++;
+    posY++;
     change_direction();
-    if (what_am_i_on == 1){
-        what_am_i_on = 0;
-    }
-    else{
-        what_am_i_on = 1;
-    }
+    im_on();
     push(-9,-9);
     push(25,-26);
     push(9,9);
@@ -92,6 +99,9 @@ void turn_left(){
 void save_coordinates(){
     visitedX[visited_index] = x;
     visitedY[visited_index] = y;
+    posXs[visited_index] = posX;
+    posYs[visited_index] = posY;
+    i_am_on[visited_index] = what_am_i_on;
     visited_index++;
 }
 
@@ -103,12 +113,11 @@ void save_coordinates_again(){
 
 void go_forth_x(){
     drive_goto(ticks,ticks);
-    printf("%d\n", posX);
     if (posX > 0){
-        x--;
+        x++;
     }
     if (posX < 0){
-        x++;
+        x--;
     }
     push(ticks,ticks);
 }
@@ -159,6 +168,10 @@ void go_back_bitch(){
     drive_goto(51,-52);
     x = visitedX[visited_index-1];
     y = visitedY[visited_index-1];
+    posY = posYs[visited_index-1];
+    posX = posXs[visited_index-1];
+    what_am_i_on = i_am_on[visited_index-1];
+    back_it_up = 1;
 }
 
 void go_fucking_bitch(){
@@ -174,7 +187,7 @@ void printer(){
     printf("shoot me\n");
     int help = x;
     int hi = y;
-    printf("co:%d, %d ",help,hi);
+    printf("co:%d, %d pos: %d, %d ",help,hi,posX,posY);
 
 }
 
@@ -185,48 +198,12 @@ void run(){
     double right_ir = find_right_ir();
 
     if (ping < 25 ){
-        drive_speed(0,0);
 
         if(left_ir > distance_from && right_ir > distance_from){
             if(is_visited_2(x,y)==1){
                 go_back_bitch();
             }
-            else if(is_visited_1(x,y)==1) {
-                save_coordinates_again();
-                if (left_ir > distance_from && right_ir <= distance_from){
-                    turn_right();
-                    go_fucking_bitch();
-                    }
-                    else{
-                        turn_left();
-                        go_fucking_bitch();
-                    }
-                }
-                else{
-                    save_coordinates();
-                    push(-100,-100);
-                    turn_right();
-                    go_fucking_bitch();
-                }
-            }
-            else if (left_ir > distance_from && right_ir <= distance_from){
-                turn_right();
-                go_fucking_bitch();
-            }
-            else if (right_ir > distance_from && left_ir <= distance_from){
-                turn_left();
-                go_fucking_bitch();
-            }
-            //dead end
-            else{
-                go_back_bitch();
-            }
-        }
-        else if(left_ir > distance_from || right_ir > distance_from ){
-            if(is_visited_2(x,y)==1){
-                go_back_bitch();
-            }
-            else if(is_visited_1(x,y)==1){
+            else if(is_visited_1(x,y)==1 && back_it_up != 0) {
                 save_coordinates_again();
                 if (left_ir > distance_from && right_ir <= distance_from){
                     turn_right();
@@ -236,17 +213,71 @@ void run(){
                     turn_left();
                     go_fucking_bitch();
                 }
-                // choose other route
-            }else{
+            }
+            else if(is_visited_1(x,y)==1){
+                printf("carsda\n");
+                save_coordinates_again();
+                go_back_bitch();
+            }
+            else{
                 save_coordinates();
                 push(-100,-100);
+                turn_right();
                 go_fucking_bitch();
             }
-            
         }
-        //going straight
+        else if (left_ir > distance_from && right_ir <= distance_from){
+            turn_right();
+            go_fucking_bitch();
+        }
+        else if (right_ir > distance_from && left_ir <= distance_from){
+            turn_left();
+            go_fucking_bitch();
+        }
+        //dead end
+        else{
+            go_back_bitch();
+        }
+    }
+    else if(left_ir > distance_from || right_ir > distance_from ){
+        if(is_visited_2(x,y)==1){
+            go_back_bitch();
+        }
+        else if(is_visited_1(x,y)==1 && back_it_up != 0){
+            save_coordinates_again();
+            if (left_ir > distance_from && right_ir <= distance_from){
+                turn_right();
+                go_fucking_bitch();
+            }
+            else{
+                turn_left();
+                go_fucking_bitch();
+            }
+
+            // choose other route
+        }
+        else if(is_visited_1(x,y)==1){
+            printf("carsda\n");
+            save_coordinates_again();
+            go_back_bitch();
+        }
+        else{
+            save_coordinates();
+            push(-100,-100);
+            go_fucking_bitch();
+        }
+            
+    }
+    //going straight
     else{
         go_fucking_bitch();
     }
+
+    if (back_it_up == 1){
+        back_it_up = 2;
+    }else if(back_it_up == 2){
+        back_it_up = 0;
+    }
+
 
 }
